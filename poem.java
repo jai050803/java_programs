dcda852885677c6117a223c6f20fea5389f1019f
jai <sjai5803@gmail.com>
2024-01-06 22:23:34 +0530

    program to print a poem

---
+++ poem.java
@@ -0,0 +1,11 @@
+public class poem {
+    public static void main(String[] args) {
+        String multilineString = "Two roads diverged in a yellow wood,\n" +
+                                "And sorry I could not travel both\n" +
+                                "And be one traveler, long I stood\n" +
+                                "And looked down one as far as I could\n" +
+                                "To where it bent in the undergrowth;\n";
+        
+        System.out.println(multilineString);
+    }
+}
