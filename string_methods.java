ff8f1bedeb4b7099aad4d78b016c395385611d94
jai <sjai5803@gmail.com>
2024-01-08 23:16:07 +0530

    string methods in java

---
+++ string_methods.java
@@ -0,0 +1,7 @@
+public class string_methods {
+    public static void main(String[] args) {
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase());
+      }
+}
