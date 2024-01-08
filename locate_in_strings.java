d2d89051a7d94286a5f847b4ee714958e85bf891
jai <sjai5803@gmail.com>
2024-01-08 23:18:16 +0530

    java program to locate a string character in string using indexof() function

---
+++ locate_in_strings.java
@@ -0,0 +1,6 @@
+public class locate_in_strings {
+    public static void main(String[] args) {
+        String txt = "Please locate where 'locate' occurs!";
+        System.out.println(txt.indexOf("locate"));
+      }
+}
