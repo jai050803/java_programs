e4289378d33d1b6ff866ed0194c6e1985c47c6a2
jai <sjai5803@gmail.com>
2024-01-08 23:14:05 +0530

    find length of a string

---
+++ string_length.java
@@ -0,0 +1,6 @@
+public class string_length {
+    public static void main(String[] args) {
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The length of the txt string is: " + txt.length());
+      }
+}
