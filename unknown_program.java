40171013ac04d98e5c403060133cc30099df8b51
jai <sjai5803@gmail.com>
2024-01-09 21:50:16 +0530

    program to find use of different increment and decreament operators in java

---
+++ unknown_program.java
@@ -0,0 +1,8 @@
+public class unknown_program {
+    public static void main(String[] args)  {              
+        int n = 4;            
+            
+        System.out.println(++n );
+        System.out.println(n);         
+    }       
+}
