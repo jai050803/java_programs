45bd86d45e6c4aa26afc698578db81429f8e9c4e
jai <sjai5803@gmail.com>
2024-01-09 22:55:00 +0530

    -m "operators"

---
+++ operators1.java
@@ -0,0 +1,15 @@
+public class operators1 {
+    public static void main(String[] args) {
+        int n;
+        System.out.println((n = 4));
+        System.out.println((n == 4));
+        System.out.println((n > 3));
+        System.out.println((n < 4));
+        System.out.println((n = 0));
+        System.out.println((n == 0));
+        System.out.println((n > 0));
+        System.out.println((n == 0 && true));
+        System.out.println((n == 4 || true));
+        System.out.println((!(n == 2)));
+    }
+}
