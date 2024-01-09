ef8f8c2b9fb6f0a96eb0d7e6ea5e6d36220d82f2
jai <sjai5803@gmail.com>
2024-01-09 23:17:29 +0530

    enum type to find color of shirt and pants

---
+++ enumColor.java
@@ -0,0 +1,10 @@
+public class enumColor {
+    enum colorType {red, orange, yellow, green, blue, violet};
+public static void main(String[] args)
+{
+    colorType shirt, pants;
+    shirt = colorType.red;
+    pants = colorType.blue;
+    System.out.println("  " + shirt + "  " + pants );
+}
+}
