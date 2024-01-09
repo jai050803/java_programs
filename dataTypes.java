6a90d2e43b55cdf9daf66e360219a36ddb106604
jai <sjai5803@gmail.com>
2024-01-09 08:12:28 +0530

    java data types

---
+++ dataTypes.java
@@ -0,0 +1,9 @@
+public class dataTypes {
+    public static void main(String[] args) {
+        boolean b = true; //boolean value true or false
+        char c = 'a'; //16-bit unicode character (single quotes)
+
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
