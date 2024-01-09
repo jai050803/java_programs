a9ccaf60593a1e103d44e6f2efe772f100028cf3
jai <sjai5803@gmail.com>
2024-01-09 22:22:59 +0530

    while loop in java

---
+++ while_loop.java
@@ -0,0 +1,10 @@
+public class while_loop {
+    public static void main(String[] args)  {
+        int n = 3;
+        while (n >= 0){
+            System.out.println( n * n );
+            --n;
+        }
+        
+    }
+}
