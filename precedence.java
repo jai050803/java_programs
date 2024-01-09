5e717670c9100adede60a252d360e3b2e94d98b9
jai <sjai5803@gmail.com>
2024-01-09 23:19:30 +0530

    program to show precedence of operators in java

---
+++ precedence.java
@@ -0,0 +1,8 @@
+public class precedence {
+    public static void main(String[] args) {
+        int i = 5, j = 6, k = 7, n = 3;
+        System.out.println( i + j * k - k % n );
+        System.out.println( i / n );
+    }
+    
+}
