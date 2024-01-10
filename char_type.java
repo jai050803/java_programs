92d16b04e9c48dc4d2e43c108b59368f34fa5867
jai <sjai5803@gmail.com>
2024-01-10 22:48:06 +0530

    char data type

--- char_type.java
+++ char_type.java
@@ -1,8 +1,8 @@
 public class char_type {
     public static void main(String[] args) {
         char [] title = {'T', 'i', 't', 'a', 'n', 'i', 'c'};
-        char ch = title[1];
-        title[3] = ch;
+        char ch = title[2];
+        title[5] = ch;
         System.out.println( title );
         System.out.println( ch );
     }
