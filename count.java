ee9f4388a835926b417cf33c636288d668652cf5
jai <sjai5803@gmail.com>
2024-01-10 22:51:07 +0530

    if-else condition in java with logical operators

---
+++ count.java
@@ -0,0 +1,8 @@
+public class count {
+    public static void main(String[] args) {
+        int found = 0, count = 5;
+        if (found != 0 || --count == 0)
+        System.out.println( "danger" );
+        System.out.println( "count = " + count );
+    }
+}
