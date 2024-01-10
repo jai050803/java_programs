ca1ccd8893c5ee7fb98295e0184166a8b7c09117
jai <sjai5803@gmail.com>
2024-01-10 23:01:20 +0530

    reverse the order of even numbers and display in ascending order

--- even_number_reverse.java
+++ even_number_reverse.java
@@ -3,7 +3,13 @@ import java.util.Scanner;
 public class even_number_reverse {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int[] num;
+        System.out.println("enter numbers separate by space");
+        String input = scan.nextLine();
+        Scanner inputScanner = new Scanner(input);
+        inputScanner.useDelimiter("[,\\s]+");
+
+
         scan.close();
+        inputScanner.close();
     }
 }
