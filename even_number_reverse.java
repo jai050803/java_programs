e82d62b2d72edbbbd053440a0098ff4be02047f0
jai <sjai5803@gmail.com>
2024-01-10 22:58:22 +0530

    reverse the order of even numbers and display in ascending order

--- even_number_reverse.java
+++ even_number_reverse.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class even_number_reverse {
     public static void main(String[] args){
-        
+        Scanner scan = new Scanner(System.in);
+        int[] num;
+        scan.close();
     }
 }
