dcf9671b265818b438cc8a07aa133c28466f9432
jai <sjai5803@gmail.com>
2024-01-10 22:35:28 +0530

    display full message

--- message_full.java
+++ message_full.java
@@ -5,7 +5,7 @@ public class message_full {
         String message;
         Scanner scan = new Scanner(System.in);
         System.out.println( "Enter a sentence on the line below." );
-        message = scan.next();
+        message = scan.nextLine();
         System.out.println( message );
         scan.close();
     }
