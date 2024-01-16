40f306206fdf4e3314d4fd7a9d6a3694d51f2b81
jai <sjai5803@gmail.com>
2024-01-16 22:01:27 +0530

    java program to print table of 1 to 25

---
+++ table.java
@@ -0,0 +1,9 @@
+public class table{
+    public static void main(String[] args) {
+        for (int i=1;i <= 25; i++){
+            for (int j=1; j<=10; j++){
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+        }
+    }
+}
