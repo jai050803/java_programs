bbe73a65aaafdddfa052af522ca98c42aed50770
jai <sjai5803@gmail.com>
2024-01-17 23:45:47 +0530

    reverse string in java

---
+++ reverseString.java
@@ -0,0 +1,12 @@
+public class reverseString {
+    public static void main(String[] args) {
+        String originalStr = "Hello";
+        String reversedStr = "";
+
+        for (int i = 0; i < originalStr.length(); i++) {
+        reversedStr = originalStr.charAt(i) + reversedStr;
+        }
+
+        System.out.println("Reversed string: "+ reversedStr);
+    }
+}
