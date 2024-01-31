bc5ed1538573e7a630183da5a92ed0968d13c965
jai <sjai5803@gmail.com>
2024-01-31 18:08:53 +0530

    java palindrome

--- palindrome.java
+++ palindrome.java
@@ -2,6 +2,9 @@ import java.util.Scanner;
 
 public class palindrome {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter a string: ");
+        String str = scanner.nextLine();
     }
 }
